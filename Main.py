b69a7055f827e99a816bd57cb2ce020f901a8908
kristoprasetya <kristoprasetya.kp@gmail.com>
2022-10-24 20:32:36 +0700

    first commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+#making a sentence
+
+adjective = input("Input your adjective: ")
+verb = input("Input your verb: ")
+other_verb = input("Input your other verb: ")
+idol = input("Your idol is: ")
+
+madlib = (f"Python is so {adjective}. It makes me very happy and excited because I love to {verb}. Don\'t give up and keep {other_verb} like you are {idol} :)")
+print(madlib)
